d628eea3d3c5f66517acdb0330568b901350683c
Mike Gulline <mike@mikegulline.com>
2024-10-30 13:29:30 -0700

    added banner videos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,9 +23,24 @@ const breadCrumbLinks = [
 ]
 
 const banners = [
-  { src: Kylie, url: '/', alt: 'Shop & Sell Kylie' },
-  { src: Pink, url: '/', alt: 'Shop & Sell Pink Friday' },
-  { src: Fenty, url: '/', alt: 'Shop & Sell Fenty' },
+  {
+    logo: Kylie,
+    src: 'https://s3-figma-videos-production-sig.figma.com/video/1007673253409586547/TEAM/b8de/217b/-e307-4355-bb4b-b58d620a5e41?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Y8SFawHxYP~r~6hftz51wVISrCqePjOM1Verh7M3tFaM9XpdVZwV5wCl6b60NCrdE~IAL9dzK2eYn4ADAd~LukzbeifC8VbB67v0I7REKmTufNDs1R50Mrq6yYKXXUnWTjJ24hfbYx-dbQ73t-lZDtigAcg~UaRRU56j3vlOS9u6kc5DWq1xZ5R3m-iriQ8VNjPcy3KW9-wEFaIeN5bNq1Levi1u7B-1QRbCu-Bm62L~-22jBIlJ7zB1GX7mOkfNAGwr~plmnJgOQJCEjm57aYJ5fxBkhMgYPdnkMn5k3Kb87AmoRotSuAGL~nA2bTHLtd3rfqn85sdOK0T3ncey9g__',
+    url: '/',
+    alt: 'Shop & Sell Kylie',
+  },
+  {
+    logo: Pink,
+    src: 'https://s3-figma-videos-production-sig.figma.com/video/1007673253409586547/TEAM/f66f/8f94/-fe7d-4628-a071-12b17cd1d388?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TN6Ct1KVlZn9hAGjdI3XRFIZcpYgAfn1ytvfdo~-X8U~29Frr9L2RrwqgIzI3KdhblmEa6FNxwAkynpvZDvHVoZdQcMK26LLu6a-YdMdIb9pYtJreZgpu8P8cuVJNvHi-qka2XBk~4IebPskZ3pEZcZCKWAgoeykfOO2nfF0EDhx1tuqAfjWY2nxI4qGumBlCqDrpe4hjOnQNEFyIXTHu3axCmWX17P8bO1fxSZfaA6topUPAX~KFKdRYLpzUrDuytOe5K7jUzsrBKRkDsFWw8rr54BNOAzMP9CjSVHbZ~FODTKa8TiPEcxtkWEhEFa~s~M2HW74zSLguZ~k~cpyKw__',
+    url: '/',
+    alt: 'Shop & Sell Pink Friday',
+  },
+  {
+    logo: Fenty,
+    src: 'https://s3-figma-videos-production-sig.figma.com/video/1007673253409586547/TEAM/0b4a/5893/-8342-4948-9f6e-05c4f052ce05?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cL0raaxnSTEACNHJrS7j3yQ7HxTo3goLObSp37414bxFpZtsqj7nnt3UNvb4mnsBWGKCyIh0Sr9QM6IDZt7OlO2KQ63EDqH8kJPlrhdcr5E9MWTXWYm1k68v41tpVvsg~~pMZPFfbfGmt-3aB5lIgu~dYUHKkXWVoJLfaCxszpyOHDjTz3wdvle3egfUSlajsiD8rtpO7JmbqKVa93nVlHqX~1i1UTnG5QZcgNaj1SoxZ~06~qeoBNo9Du0T6fSOnbiNs749ApvtkvhX87bZWNwm3UnZyGFXrjbMA3a1qmLzUY1YgWLpx0xjZCn~-bDKXBuHV3f5OB51tGop6v0zNA__',
+    url: '/',
+    alt: 'Shop & Sell Fenty',
+  },
 ]
 
 const whatYouLove = [
@@ -55,10 +70,18 @@ export default function Home() {
       <main className={styles['home']}>
         <section className={styles['banners']}>
           <ul>
-            {banners.map(({ src, alt, url }) => (
+            {banners.map(({ logo, src, alt, url }) => (
               <li key={alt}>
                 <Link href={url} title={alt}>
-                  <Image src={src} width={480} height={574} alt={alt} />
+                  <video autoPlay muted loop>
+                    <source src={src} type="video/mp4" />
+                  </video>
+                  <div className={styles['overlay']}>
+                    <div>
+                      <p>Shop &amp; Sell</p>
+                      <Image src={logo} width={408} height={100} alt={alt} />
+                    </div>
+                  </div>
                 </Link>
               </li>
             ))}
